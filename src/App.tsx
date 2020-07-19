ca0ed0bd52573b8210f34ba5116d80bab76deb5b
Simon Buysse <buysse.simon@gmail.com>
2020-07-19 21:05:39 +0200

    Add ch02 instructions

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Header } from "./components";
 
 export function App() {
-  return (
-    <div className="w-full h-full">
-      <Header
-        title="Hello, jworks!"
-        handleClickMenuButton={() => console.log('the menu button was clicked')}
-      ></Header>
-    </div>
-  );
+    return (
+        <div className="w-full h-full">
+            <Header
+                title="Hello, jworks!"
+                handleClickMenuButton={() => console.log('the menu button was clicked')}
+            ></Header>
+        </div>
+    );
 }
 
 export default App;

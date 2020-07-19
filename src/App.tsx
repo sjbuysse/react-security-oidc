440c0a44fd7ed65af8ef4759fd06cbb9d640cf9b
Simon Buysse <buysse.simon@gmail.com>
2020-07-19 22:31:07 +0200

    toggle sidenav and add instructions for ch03

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Header } from "./components";
+import { Sidenav, SidenavContainer, SidenavContent } from './components/Sidenav/Sidenav';
 
 export function App() {
+    const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false);
     return (
         <div className="w-full h-full">
-            <Header
-                title="Hello, jworks!"
-                handleClickMenuButton={() => console.log('the menu button was clicked')}
-            ></Header>
+            <Header title='Jworks base app' handleClickMenuButton={() => setIsSidebarOpen(!isSidebarOpen)} />
+            <SidenavContainer>
+                <Sidenav isSidenavOpen={isSidebarOpen}>
+                    <div>Item 1</div>
+                    <div>Item 1</div>
+                </Sidenav>
+                <SidenavContent>
+                </SidenavContent>
+            </SidenavContainer>
         </div>
     );
 }

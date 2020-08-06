427bb6fccd3d6189c9dd0c1ab217e11c09beb018
Simon Buysse <buysse.simon@gmail.com>
2020-08-06 17:59:33 +0200

    remove react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,10 @@ import './index.css';
 import './assets/tailwind.generated.css'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
+        <App/>
     </React.StrictMode>,
     document.getElementById('root')
 );

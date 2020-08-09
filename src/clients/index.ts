c2d274c2e4adb4dc422ecbe6c6b7b2bc2ad6761a
Simon Buysse <buysse.simon@gmail.com>
2020-08-09 12:02:39 +0200

    Add solution to ch04

--- src/clients/index.ts
+++ src/clients/index.ts
@@ -1,5 +1,9 @@
+<<<<<<< HEAD
 export * from "./containers";
 export * from "./components";
 export * from "./model";
 export * from "./services";
 export * from "./pages";
+=======
+export * from './pages/ClientsPage/ClientsPage';
+>>>>>>> Add solution to ch04

48eba2a0af11a552ac351799930604097403ad2e
Simon Buysse <buysse.simon@gmail.com>
2020-08-09 18:26:24 +0200

    Add routes to clientspage

--- src/clients/pages/ClientsPage/ClientsPage.tsx
+++ src/clients/pages/ClientsPage/ClientsPage.tsx
@@ -1,5 +1,36 @@
 import React from 'react';
+import {
+    Switch,
+    Route,
+    useRouteMatch,
+    match,
+    useHistory,
+} from "react-router-dom";
+import { Page } from '../../../components';
+import { ClientsTable } from '../../containers/ClientsTable/ClientsTable';
 
-export const ClientsPage = () => (
-    <h2>Here comes the clients page</h2>
-)
+export function ClientsPage() {
+    const { url, path }: match = useRouteMatch();
+    const { push } = useHistory();
+    const gotToCreateClient = () => push(`${url}/create`);
+
+    return (
+        <Switch>
+            <Route exact path={path}>
+                <Page title="Clients" onCreateButtonClick={gotToCreateClient}>
+                    <ClientsTable></ClientsTable>
+                </Page>
+            </Route>
+            <Route path={`${path}/create`}>
+                <Page title="Create client" onCreateButtonClick={gotToCreateClient}>
+                    Here will come the CreateClient container
+                </Page>
+            </Route>
+            <Route path={`${path}/:clientId/edit`}>
+                <Page title="Edit client" onCreateButtonClick={gotToCreateClient}>
+                    Here will come the EditClient container
+                </Page>
+            </Route>
+        </Switch>
+    );
+}

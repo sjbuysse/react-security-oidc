e7a7bfd01c22add8da059d04aecb3eaba76c351a
Simon Buysse <buysse.simon@gmail.com>
2020-08-12 20:18:47 +0200

    sidenav shouldn't overflow y-axis

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,18 @@ import { Sidenav, SidenavContainer, SidenavContent } from './components/Sidenav/
 export function App() {
     const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false);
     return (
-        <div className="w-full h-full">
+        <div className="w-full h-full flex flex-col">
             <Header title='Jworks base app' handleClickMenuButton={() => setIsSidebarOpen(!isSidebarOpen)} />
-            <SidenavContainer>
-                <Sidenav isSidenavOpen={isSidebarOpen}>
-                    <div>Item 1</div>
-                    <div>Item 1</div>
-                </Sidenav>
-                <SidenavContent>
-                </SidenavContent>
-            </SidenavContainer>
+            <div className="flex flex-1 flex-auto">
+                <SidenavContainer>
+                    <Sidenav isSidenavOpen={isSidebarOpen}>
+                        <div>Item 1</div>
+                        <div>Item 1</div>
+                    </Sidenav>
+                    <SidenavContent>
+                    </SidenavContent>
+                </SidenavContainer>
+            </div>
         </div>
     );
 }

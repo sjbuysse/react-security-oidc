7ada1193dd43b45c531c5efa2dfb22e8de804203
Simon Buysse <buysse.simon@gmail.com>
2020-08-14 21:59:15 +0200

    update products

--- src/products/containers/EditProduct/EditProduct.tsx
+++ src/products/containers/EditProduct/EditProduct.tsx
@@ -1,33 +1,35 @@
 import React, { useState, useEffect } from "react";
 import { match, useRouteMatch, useHistory } from "react-router-dom";
 import { Product, getProduct, editProduct, ProductForm } from "products";
+import { getClient } from '../../../clients/services';
 
 export function EditProduct() {
     const {
-        params: { productId },
+        params: {productId},
     }: match<{ productId: string }> = useRouteMatch();
-    const { push } = useHistory();
+    const {push} = useHistory();
     const [product, setProduct] = useState<Product | undefined>(undefined);
     const goBackToProducts = () => push("/products");
+    const onEditProduct = async (productFields: Partial<Product>) => {
+        await editProduct({...product!, ...productFields});
+        goBackToProducts();
+    };
     const retrieveProduct = async () => {
         const result = await getProduct(productId);
         setProduct(result);
     };
-    const onEditProduct = async (productFields: Partial<Product>) => {
-        await editProduct({ ...product!, ...productFields });
-        goBackToProducts();
-    };
 
     useEffect(() => {
+        console.log('hier');
         retrieveProduct();
         // eslint-disable-next-line
     }, []);
 
     return product ? (
-            <ProductForm
-                product={product}
-                onSubmit={onEditProduct}
-                onCancel={goBackToProducts}
-            ></ProductForm>
+        <ProductForm
+            product={product}
+            onSubmit={onEditProduct}
+            onCancel={goBackToProducts}
+        ></ProductForm>
     ) : null;
 }
--- src/products/services/products.tsx
+++ src/products/services/products.tsx
@@ -1,49 +1,39 @@
 import { Product } from "products/types";
 
-const apiUrl = "http://localhost:8080/products/";
+const apiUrl = "http://localhost:8080/products";
 
-export const getProducts = async () => {
-    const response = await fetch(apiUrl);
+export const getProduct = async (id: string): Promise<Product> =>
+    (await fetch(`${apiUrl}/${id}`)).json();
 
-    return response.json();
-};
+export const getProducts = async (): Promise<Product[]> =>
+    (await fetch(apiUrl)).json();
 
-export const getProduct = async (id: string) => {
-    const response = await fetch(`${apiUrl}{id}`);
+export const createProduct = async (
+    product: Partial<Product>
+): Promise<Product> =>
+    (
+        await fetch(apiUrl, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(product),
+        })
+    ).json();
 
-    return response.json();
-};
+export const editProduct = async (product: Product): Promise<Product> =>
+    (
+        await fetch(`${apiUrl}/${product.id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(product),
+        })
+    ).json();
 
-export const addProduct = async (
-    product: Partial<Product> = {}
-): Promise<Product[]> => {
-    const response = await fetch(apiUrl, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(product),
-    });
-
-    return response.json();
-};
-
-export const editProduct = async (product: Product): Promise<Product[]> => {
-    const response = await fetch(`${apiUrl}${product.id}`, {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(product),
-    });
-
-    return response.json();
-};
-
-export const deleteProduct = async (productId: string): Promise<{}> => {
-    await fetch(`${apiUrl}${productId}`, {
+export const deleteProduct = async (id: string): Promise<any> => {
+    return await fetch(`${apiUrl}/${id}`, {
         method: "DELETE",
     });
-
-    return {};
 };

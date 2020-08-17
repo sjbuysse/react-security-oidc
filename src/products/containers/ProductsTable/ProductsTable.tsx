c09bde268be3ba0e5fa0df9385c8ad96335af34c
Simon Buysse <buysse.simon@gmail.com>
2020-08-17 22:57:52 +0200

    retrieve products on location change

--- src/products/containers/ProductsTable/ProductsTable.tsx
+++ src/products/containers/ProductsTable/ProductsTable.tsx
@@ -1,50 +1,47 @@
-import React from "react";
-import {
-    Switch,
-    Route,
-    useRouteMatch,
-    match,
-    useHistory,
-} from "react-router-dom";
-import { Page } from "components";
-import { ProductsTable } from "products";
-import { EditProduct, CreateProduct } from "products/containers";
-import { Dialog } from '../../../components/Dialog/Dialog';
+import React, { useState, useEffect } from "react";
+import { match, useRouteMatch, useHistory, useLocation } from "react-router-dom";
+import { Product, getProducts, deleteProduct } from "products";
+import { Table } from "components";
 import { useSelector } from 'react-redux';
 import { selectIsReadOnly } from '../../../state/selectors';
 
-export function ProductsPage() {
-    const {url}: match = useRouteMatch();
+export function ProductsTable() {
+    const [products, setProducts] = useState<Product[] | undefined>(undefined);
+    const { url }: match = useRouteMatch();
+    const location = useLocation();
+    const history = useHistory();
     const isReadOnly = useSelector(selectIsReadOnly);
-    const {push} = useHistory();
 
-    const gotToCreateProduct = () => push(`${url}/create`);
+    useEffect(() => {
+        return history.listen(() => retrieveProducts())
+    },[history]);
 
+    const retrieveProducts = async () => {
+        const result = await getProducts();
+        setProducts(result);
+    };
+
+    const onDeleteProduct = async (id: string) => {
+        if (window.confirm("Are you sure?")) {
+            await deleteProduct(id);
+            retrieveProducts();
+        }
+    };
+
+    useEffect(() => {
+        retrieveProducts();
+    }, [history]);
 
     return (
-        <>
-            <Page title="Products" onCreateButtonClick={isReadOnly ? undefined : gotToCreateProduct}>
-                <ProductsTable></ProductsTable>
-            </Page>
-            <Switch>
-                <Route path={`${url}/create`}>
-                    <Dialog show={true} title={'Create product'}
-                            onClose={() => push(url)}>
-                        {isReadOnly
-                            ? <span>The read-only setting is activated</span>
-                            : <CreateProduct></CreateProduct>}
-                    </Dialog>
-                </Route>
-                <Route path={`${url}/:productId/edit`}>
-                    <Dialog show={true} title={'Edit product'}
-                            onClose={() => push(url)}>
-                        {isReadOnly
-                            ? <span>The read-only setting is activated</span>
-                            : <EditProduct></EditProduct>}
-                    </Dialog>
-                </Route>
-            </Switch>
-        </>
+        <Table
+            headers={["Id", "Name", "Description", "Product Code"]}
+            isReadOnly={isReadOnly}
+            data={products}
+            onEdit={(productid) => {
+                history.push(`${url}/${productid}/edit`, {background: location})
+            }}
+            onDelete={onDeleteProduct}
+        ></Table>
     );
 }
 

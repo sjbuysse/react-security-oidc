213e72f5975a524de9d5c6efb22bba9050ace71d
Simon Buysse <buysse.simon@gmail.com>
2020-10-17 23:06:12 +0200

    merge conflicts resolve

--- src/clients/services/clients.ts
+++ src/clients/services/clients.ts
@@ -5,9 +5,6 @@ const apiUrl = "https://base-app-backend.herokuapp.com/clients";
 export const getClient = async (id: string): Promise<Client> =>
     (await fetch(`${apiUrl}/${id}`)).json();
 
-export const getClient = async (id: string): Promise<Client> =>
-    (await fetch(`${apiUrl}/${id}`)).json();
-
 export const getClients = async (): Promise<Client[]> =>
   (await fetch(apiUrl)).json();
 

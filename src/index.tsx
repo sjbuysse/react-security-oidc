1a2aa5a8a572d88981a77c9b2246072f5408e88c
Simon Buysse <buysse.simon@gmail.com>
2020-11-06 00:01:19 +0100

    Add ch11.md

--- src/index.tsx
+++ src/index.tsx
@@ -18,7 +18,7 @@ ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
             <Provider store={store}>
-            <App/>
+                <App/>
             </Provider>
         </BrowserRouter>
     </React.StrictMode>,

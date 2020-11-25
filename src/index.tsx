0d1e3c5425aa176795d327b3e7f1df56c440c60c
Simon Buysse <buysse.simon@gmail.com>
2020-11-25 19:26:53 +0100

    update ch11.md

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import './assets/tailwind.generated.css'
 import App from './App';
-import {Provider} from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import { configureStore } from '@reduxjs/toolkit';
+import {Provider} from 'react-redux';
 import { appReducer, featureKey as appFeature} from './state/reducer';
 
 const store = configureStore({

6cbeef7a916b61d3c2bb092bd6fccf8569757c9d
simon <buysse.simon@gmail.com>
2021-09-17 17:42:24 +0200

    typo and remove old tailwind solution

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,25 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: '#ebf8ff',
+          200: '#bee3f8',
+          300: '#E37C30',
+          400: '#63b3ed',
+          500: '#E7904B',
+          600: '#3182ce',
+          700: '#EAB285',
+          800: '#2c5282',
+          900: '#F7D1A7',
+        },
+      }
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+    },
   },
   plugins: [],
 }
